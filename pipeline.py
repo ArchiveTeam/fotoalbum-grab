873dde031163e42f71c088633249f53124e4833e
arkiver <arkiver@protonmail.com>
2020-10-31 01:15:47 +0100

    Version 20201031.01. Support Wget-AT version .20.3-at.20201030.01.

--- pipeline.py
+++ pipeline.py
@@ -36,7 +36,10 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 
 WGET_AT = find_executable(
     'Wget+AT',
-    ['GNU Wget 1.20.3-at.20200919.01'],
+    [
+        'GNU Wget 1.20.3-at.20200919.01',
+        'GNU Wget 1.20.3-at.20201030.01'
+    ],
     ['./wget-at']
 )
 
@@ -49,7 +52,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201015.02'
+VERSION = '20201031.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'fotoalbum'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
